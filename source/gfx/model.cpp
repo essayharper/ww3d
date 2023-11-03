a68ae7f2d0fa193250dd2b7189eee8caf487cc8d
Lollio64 <141979356+Lollio64@users.noreply.github.com>
2023-11-03 17:41:41 +0100

    W.I.P model renderer in model.cpp

--- source/gfx/model.cpp
+++ source/gfx/model.cpp
@@ -6,10 +6,23 @@
 
 #include <enums.h>
 #include <citro3d.h>
+#include <tex3ds.h>
 #include <vshader_shbin.h>
 #include <gfx/model.h>
+#include <gfx/gfx.h>
 
-model::model(std::string filename) {
+// Helper function for loading a texture from memory
+static bool loadTextureFromMem(C3D_Tex* texture, C3D_TexCube* cube, const void* data, size_t size) {
+	Tex3DS_Texture t3x = Tex3DS_TextureImport(data, size, tex, cube, false);
+	if (!t3x) {
+		return false;
+    };
+    // Delete the t3x object since we don't need it
+	Tex3DS_TextureFree(t3x);
+	return true;
+};
+
+model::model(std::string filename, std::string texname) {
     // Open the file
     std::ifstream mdlfFile(filename);
 
@@ -42,6 +55,24 @@ model::model(std::string filename) {
 };
 
 void model::render(gfx::GFX &gfx) {
+    // Configure attributes for use with the vertex shader
+	C3D_AttrInfo* attrInfo = C3D_GetAttrInfo();
+	AttrInfo_Init(attrInfo);
+	AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 3); // V0 = Position
+	AttrInfo_AddLoader(attrInfo, 1, GPU_FLOAT, 2); // V1 = Texcoords
+	AttrInfo_AddLoader(attrInfo, 2, GPU_FLOAT, 3); // V2 = Normals
+
+    // Configure buffers
+	C3D_BufInfo* bufInfo = C3D_GetBufInfo();
+	BufInfo_Init(bufInfo);
+	BufInfo_Add(bufInfo, vbo_data, sizeof(vertex), 3, 0x210);
+
+	C3D_TexEnv* env = C3D_GetTexEnv(0);
+	C3D_TexEnvInit(env);
+    C3D_TexBind(0, &texture);
+	C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_PRIMARY_COLOR, 0);
+	C3D_TexEnvFunc(env, C3D_Both, GPU_MODULATE);
+
     // Render Vertex Array, if the Index Array is empty
     if(model::mdlfIndices.empty()) {
         C3D_DrawArrays(GPU_TRIANGLES, 0, mdlfVertices.size());
