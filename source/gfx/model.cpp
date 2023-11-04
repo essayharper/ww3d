5e2d454f37079c2223c7ebdd05cfe095886c1e1d
Lollio64 <141979356+Lollio64@users.noreply.github.com>
2023-11-04 20:12:26 +0100

    Small code fixes in model.cpp

--- source/gfx/model.cpp
+++ source/gfx/model.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 
+#include <3ds.h>
 #include <enums.h>
 #include <citro3d.h>
 #include <tex3ds.h>
@@ -12,8 +13,8 @@
 #include <gfx/gfx.h>
 
 // Helper function for loading a texture from memory
-static bool loadTextureFromMem(C3D_Tex* texture, C3D_TexCube* cube, const void* data, size_t size) {
-	Tex3DS_Texture t3x = Tex3DS_TextureImport(data, size, texture, cube, false);
+static bool loadTexFromMem(C3D_Tex* texData, C3D_TexCube* cube, const void* data, size_t size) {
+	Tex3DS_Texture t3x = Tex3DS_TextureImport(data, size, texData, cube, false);
 	if (!t3x) {
 		return false;
     };
@@ -26,29 +27,29 @@ model::model(std::string filename) {
     // Open the file
     std::ifstream mdlfFile(filename);
 
-    while (std::getline(mdlfFile, model::readLine)) {
+    while (std::getline(mdlfFile, readLine)) {
             // Get line prefix
-            model::ss.clear();
-            model::ss.str(readLine);
-            model::ss >> model::prefix;
+            ss.clear();
+            ss.str(readLine);
+            ss >> prefix;
  
-        if (model::prefix == "#") {
+        if (prefix == "#") {
             // Do nothing
-        } else if (model::prefix == "v") {
+        } else if (prefix == "v") {
             ss >> pos.x >> pos.y >> pos.z >> comment >> tex.u >> tex.v >> comment >> nrm.x >> nrm.y >> nrm.z;
-            model::mdlfPosition.push_back(pos.x);
-            model::mdlfPosition.push_back(pos.y);
-            model::mdlfPosition.push_back(pos.z);
-            model::mdlfTexcoords.push_back(tex.u);
-            model::mdlfTexcoords.push_back(tex.v);
-            model::mdlfNormals.push_back(nrm.x);
-            model::mdlfNormals.push_back(nrm.y);
-            model::mdlfNormals.push_back(nrm.z);
-        } else if (model::prefix == "i") {
-            ss >> model::tempIdxA >> model::tempIdxB >> model::tempIdxC;
-            model::mdlfIndices.push_back(model::tempIdxA);
-            model::mdlfIndices.push_back(model::tempIdxB);
-            model::mdlfIndices.push_back(model::tempIdxC);
+            mdlfPosition.push_back(pos.x);
+            mdlfPosition.push_back(pos.y);
+            mdlfPosition.push_back(pos.z);
+            mdlfTexcoords.push_back(tex.u);
+            mdlfTexcoords.push_back(tex.v);
+            mdlfNormals.push_back(nrm.x);
+            mdlfNormals.push_back(nrm.y);
+            mdlfNormals.push_back(nrm.z);
+        } else if (prefix == "i") {
+            ss >> tempIdxA >> tempIdxB >> tempIdxC;
+            mdlfIndices.push_back(tempIdxA);
+            mdlfIndices.push_back(tempIdxB);
+            mdlfIndices.push_back(tempIdxC);
         };
     };
    mdlfFile.close(); 
@@ -69,15 +70,18 @@ void model::render(gfx::GFX &gfx) {
 
 	C3D_TexEnv* env = C3D_GetTexEnv(0);
 	C3D_TexEnvInit(env);
-    C3D_TexBind(0, &gfx::GFX::texture);
-	C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_PRIMARY_COLOR, GPU_FRAGMENT_SECONDARY_COLOR);
+    if (!loadTexFromMem(&texData, &cube, &data, size)) {
+        svcBreak(USERBREAK_PANIC);
+    };
+    C3D_TexBind(0, &texData);
+	C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_PRIMARY_COLOR, 0);
 	C3D_TexEnvFunc(env, C3D_Both, GPU_MODULATE);
 
     // Render Vertex Array, if the Index Array is empty
-    if(model::mdlfIndices.empty()) {
+    if(mdlfIndices.empty()) {
         C3D_DrawArrays(GPU_TRIANGLES, 0, mdlfVertices.size());
     } else {
-        C3D_DrawElements(GPU_TRIANGLES, model::mdlfIndices.size(), C3D_UNSIGNED_SHORT, model::mdlfIndices.data());
+        C3D_DrawElements(GPU_TRIANGLES, mdlfIndices.size(), C3D_UNSIGNED_SHORT, mdlfIndices.data());
     };
 };
 
@@ -88,4 +92,10 @@ model::~model() {
     mdlfNormals.clear();
     mdlfIndices.clear();
     mdlfVertices.clear();
+
+    // Free the texture
+    C3D_TexDelete(&texData);
+
+    // Free the Vertex Buffer Object
+    linearFree(gfx::GFX::vbo_data);
 };
