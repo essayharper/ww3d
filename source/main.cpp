73b4930bae95f97d8dee4cfb1f2d6ea2f735bb82
Lollio64 <141979356+Lollio64@users.noreply.github.com>
2023-11-04 20:11:01 +0100

    More debug logs and WIP scene rendering in main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -9,10 +9,11 @@
 #include <citro2d.h>
 
 // Insert custom header files here
+#include <gfx/gfx.h>
+#include <gfx/model.h>
 
 // Define some stuff
 #define CLEAR_COLOR 0x68B0D8FF
-
 #define DISPLAY_TRANSFER_FLAGS (GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
 
 // MAIN FUNCTION
@@ -23,31 +24,45 @@ int main() {
     // Init graphics
     gfxInitDefault();
     consoleInit(GFX_BOTTOM, NULL); // Debug console output
+    std::cout << "Initializing graphics..." << "\n";
     C3D_Init(C3D_DEFAULT_CMDBUF_SIZE); // 3D Graphics
     C2D_Init(C2D_DEFAULT_MAX_OBJECTS); // 2D Graphics
-
+    std::cout << "Initialized graphics successfully!" << "\n";
+ 
+    std::cout << "Initialize render target..." << "\n"; 
     // Initialize a render target for the top screen to tell Citro3D to which screen to render to 
     C3D_RenderTarget* leftTarget = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
     C3D_RenderTargetSetOutput(leftTarget, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
+    std::cout << "Initialized render target successfully!" << "\n";
 
-    // Print stuff
-    std::cout << "Initializing Scene..." << "\n";
+    // Instantiate some objects. move to scenes
+    std::cout << "Instantiate class objects..." << "\n";
+    gfx::GFX gfx;
+    model testMDL("romfs:/models/test.mdlf");
+    std::cout << "Instantiated objects successfully!" << "\n";
 
     // Main Loop
     while(aptMainLoop()) {
-        // Allow fo user input
+        // Scan user input every frame
         hidScanInput();
         
-        // Respond to user input
+        // Check if a key has been pressed
         u32 kDown = hidKeysDown();
 
         if (kDown & KEY_START) {
             break; // Break in order to return to the Homebrew Launcher, Debug only
         }
+
+        // Render the scene
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C3D_RenderTargetClear(leftTarget, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
+		C3D_FrameDrawOn(leftTarget);
+		testMDL.render();
+		C3D_FrameEnd(0);
     };
 
     // Print stuff
-    std::cout << "Deinitializing Scene..." << "\n";
+    std::cout << "Deinitializing graphics..." << "\n";
      
     // Deinit romfs
     romfsExit();
@@ -55,6 +70,7 @@ int main() {
     // Deinit graphics
     C2D_Fini();
     C3D_Fini();
+    std::cout << "Deinitialized graphics successfully!" << "\n";
     gfxExit();
     return 0;
 };
